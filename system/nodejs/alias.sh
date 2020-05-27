8f949ba6663983e76288e16c69868700e78596a4
Erko Bridee <erko.bridee@gmail.com>
2020-05-27 19:08:10 +0200

    update(alias): nodejs
    do not index the content of  node_modules folder on the macos

--- system/nodejs/alias.sh
+++ system/nodejs/alias.sh
@@ -1,2 +1,4 @@
 alias npmri='rm -rf node_modules && npm install'
 alias yarnri='rm -rf node_modules && yarn'
+
+alias npm_ignore_indexing='find . -type d -name "node_modules" -exec touch "{}/.metadata_never_index" \; -exec echo "✔ {}" \;'
