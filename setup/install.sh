b4d9c83c804ce22839c48d00b94b50d4faaed9cb
erkobridee <erko.bridee@gmail.com>
2022-05-17 17:35:07 +0200

    update(macos): setup flow

--- setup/install.sh
+++ setup/install.sh
@@ -2,20 +2,20 @@ INSTALL_DIR=$DOTFILES_DIR/$(dirname $BASH_SOURCE)/install
 
 setup_chapter "install brew utilities"
 
-brew bundle --file=$(INSTALL_DIR)/Brewfile || true
+setup_run brew bundle --file=$(INSTALL_DIR)/Brewfile || true
 
 setup_chapter "install fonts"
 
-brew bundle --file=$(INSTALL_DIR)/CaskFontsFile || true
+setup_run brew bundle --file=$(INSTALL_DIR)/CaskFontsFile || true
 
 setup_chapter "install apps"
 
-brew bundle --file=$(INSTALL_DIR)/CaskAppsFile || true
+setup_run brew bundle --file=$(INSTALL_DIR)/CaskAppsFile || true
 
-xattr -d -r com.apple.quarantine ~/Library/QuickLook
+setup_run xattr -d -r com.apple.quarantine ~/Library/QuickLook
 
 setup_chapter "install VS Code cloud sync extension"
 
-code --install-extension Shan.code-settings-sync
+setup_run code --install-extension Shan.code-settings-sync
 
 unset INSTALL_DIR
--- setup/node.sh
+++ setup/node.sh
@@ -5,10 +5,10 @@ if is_executable nvm; then
   print_success 'nvm is already setup'
 else
   print_info 'setting up the nvm'
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+  setup_run curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
   print_info 'installing the node.js v16 (lts/gallium)'
-  nvm install --lts=gallium
+  setup_run nvm install --lts=gallium
 fi
 
 print_info "node --version $(node --version)"
